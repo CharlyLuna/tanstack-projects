d3bb1a1dc448926e85c947184610fe3e8c2d8d12
CharlyLuna <cluna10@ucol.mx>
2024-05-28 14:00:49 -0600

    Add selection of labels

--- react-query-issues-main/src/issues/components/LabelPicker.tsx
+++ react-query-issues-main/src/issues/components/LabelPicker.tsx
@@ -1,7 +1,12 @@
 import { LoadingIcon } from "../../shared/components/LoadingIcon"
 import { useLabels } from "../hooks/useLabels"
 
-export const LabelPicker = () => {
+interface Props {
+  selectedLabels: string[]
+  onChange: (labelName: string) => void
+}
+
+export const LabelPicker = ({ selectedLabels, onChange }: Props) => {
   const labelsQuery = useLabels()
 
   if (labelsQuery.isLoading) return <LoadingIcon />
@@ -11,10 +16,14 @@ export const LabelPicker = () => {
       {labelsQuery.data?.map((label) => (
         <span
           key={label.id}
+          onClick={() => onChange(label.name)}
           className='badge rounded-pill m-1 label-picker'
           style={{
             border: `1px solid #${label.color}`,
             color: `#${label.color}`,
+            backgroundColor: selectedLabels.includes(label.name)
+              ? `#${label.color}7f`
+              : "",
           }}
         >
           {label.name}
