fc97d464f2e1e4c11be0fcf79da2c79212f649a2
CharlyLuna <cluna10@ucol.mx>
2024-06-03 16:03:38 -0600

    Add axios and create interfaces as well as function to retrieve data

---
+++ react-query-store-main/src/products/services/actions.ts
@@ -0,0 +1,11 @@
+import { productsApi } from "../api/productsApi"
+import type { Product } from "../interfaces/product"
+
+interface GetProductsOptions {
+  filterKey?: string
+}
+
+export const getProducts = async ({ filterKey }: GetProductsOptions) => {
+  const { data } = await productsApi.get<Product[]>("/products")
+  return data
+}
